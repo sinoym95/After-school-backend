6e4f905c85947c97d40312e1ff2c45afbd748b59
sinoym95 <140094055+sinoym95@users.noreply.github.com>
2025-07-06 12:42:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const lessons = [
   { id: 6, title: "English Class", description: "Improve language skills", price: 60, availableStock: 13, location: "Liverpool" },
   { id: 7, title: "Sport Science Class", description: "Stay active and fit", price: 60, availableStock: 14, location: "Glasgow" },
   { id: 8, title: "Computer Science Class", description: "Learn coding and programming", price: 60, availableStock: 15, location: "Bristol" },
-  { id: 9, title: "History Class", description: "Learn history fundamentals", price: 65, availableStock: 4, location: "Nottingham" },
+  { id: 9, title: "History Class", description: "Learn history fundamentals", price: 69, availableStock: 23, location: "Nottingham" },
   { id: 10, title: "Maths Class", description: "Learn advanced math", price: 75, availableStock: 3, location: "Leicester" }
 ];
 
