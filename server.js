32410294c95331f28348a1370975732cd6998d03
sinoym95 <140094055+sinoym95@users.noreply.github.com>
2025-07-06 12:41:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const lessons = [
   { id: 5, title: "Physics Class", description: "Discover the laws of nature", price: 60, availableStock: 12, location: "Sheffield" },
   { id: 6, title: "English Class", description: "Improve language skills", price: 60, availableStock: 13, location: "Liverpool" },
   { id: 7, title: "Sport Science Class", description: "Stay active and fit", price: 60, availableStock: 14, location: "Glasgow" },
-  { id: 8, title: "Computer Science Class", description: "Learn coding and programming", price: 70, availableStock: 5, location: "Bristol" },
+  { id: 8, title: "Computer Science Class", description: "Learn coding and programming", price: 60, availableStock: 15, location: "Bristol" },
   { id: 9, title: "History Class", description: "Learn history fundamentals", price: 65, availableStock: 4, location: "Nottingham" },
   { id: 10, title: "Maths Class", description: "Learn advanced math", price: 75, availableStock: 3, location: "Leicester" }
 ];
