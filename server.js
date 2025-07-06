44c66f8ac3975a0ed59c52968f8a406c969134d7
sinoym95 <140094055+sinoym95@users.noreply.github.com>
2025-07-06 12:28:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 // Dummy data: A list of lessons
 const lessons = [
   { id: 1, title: "Art Class", description: "Fun art class", price: 50, availableStock: 7, location: "London" },
-  { id: 2, title: "Biology Class", description: "Fun biology adventures", price: 60, availableStock: 3, location: "Birmingham" },
+  { id: 2, title: "Biology Class", description: "Fun biology adventures", price: 60, availableStock: 9, location: "Birmingham" },
   { id: 3, title: "Business Class", description: "Learn business fundamentals", price: 40, availableStock: 7, location: "Manchester" },
   { id: 4, title: "Chemistry Class", description: "Explore chemical reactions", price: 55, availableStock: 4, location: "Leeds" },
   { id: 5, title: "Physics Class", description: "Discover the laws of nature", price: 45, availableStock: 6, location: "Sheffield" },
